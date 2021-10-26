45b8021be8f97c46c1c327e581b30322f6d53bea
Tymoteusz Urbanowicz <dym7774@gmail.com>
2021-10-26 21:59:45 +0200

    mostly finished lab3

--- lab3/annealing.cpp
+++ lab3/annealing.cpp
@@ -19,7 +19,8 @@ std::ostream& operator<<(std::ostream& o, vector<double> v)
     return o;
 }
 
-vector<double> annealing(function<double(vector<double>)> f, function<bool(vector<double>)> f_domain, vector<double> p0, vector<double> next_p, int iterations)
+vector<double> hillclimb(function<double(vector<double>)> f, function<bool(vector<double>)> f_domain, 
+    vector<double> p0, vector<double> next_p, int iterations)
 {
     auto p_current = p0;
     std::uniform_int_distribution<> distrib(0, p_current.size() - 1);
@@ -43,9 +44,52 @@ vector<double> annealing(function<double(vector<double>)> f, function<bool(vecto
     return p_current;
 }
 
+vector<double> annealing(function<double(vector<double>)> f, function<bool(vector<double>)> f_domain, 
+    vector<double> p0, int iterations, 
+    function<vector<double>(vector<double>)> norm, function<double(int)> temp)
+{
+    auto s_current = p0;
+    auto s_best = p0;
+
+    std::uniform_real_distribution<> u_k(0.0, 1.0);
+
+    if (!f_domain(s_current)) throw std::invalid_argument("The p0 point must be in domain");
+
+    for (int i = 0; i < iterations; i++) {
+        vector<double> s_new;
+        do {
+            s_new = norm(s_current);
+        }
+        while(!f_domain(s_new));
+        if (f(s_new) < f(s_current)) {
+            s_current = s_new;
+        } else {
+            double u = u_k(gen);
+            if (u < std::exp(-std::abs(f(s_new) - f(s_current)) / temp(i))) {
+                s_current = s_new;
+            }
+        }
+        if (f(s_current) < f(s_best)) {
+            s_best = s_current;
+        }
+        //std::cout << s_current << " -> " << f(s_current) << std::endl;
+    }
+    return s_best;
+}
+
 int main() {
 
+    auto temperature = [](int k){
+        return 1000.0 / k;
+    };
 
+    auto normal = [](auto p) {
+            std::normal_distribution<double> n(0.0, 0.3);
+            for (auto& e : p) {
+                e = e + n(gen);
+            }
+            return p;
+    };
     auto rastrigin = [](vector<double> v){
         auto n = (double)v.size();
         double a = 10.0;
@@ -82,28 +126,45 @@ int main() {
         eggholder_dist(gen)
     };
 
-    vector<double> next_p_dist = {-0.01, 0.01};
+    vector<double> next_p_dist = {-0.1, 0.1};
 
-    int input = 0;
+    int f_input = 0;
     std::cout << "Choose function to optimize:" << std::endl
             << "    [1] Rastrigin function" << std::endl
             << "    [2] Eggholder function  ";
-    if(!(std::cin >> input) || input < 1 || input > 2) throw std::logic_error("Invalid input");
+    if(!(std::cin >> f_input) || f_input < 1 || f_input > 2) throw std::logic_error("Invalid input");
+
+    int a_input = 0;
+    std::cout << "Choose algorithm for optimization:" << std::endl
+            << "    [1] Hillclimb" << std::endl
+            << "    [2] Simulated annealing  ";
+    if(!(std::cin >> a_input) || a_input < 1 || a_input > 2) throw std::logic_error("Invalid input");
 
     int iterations = 0;
     std::cout << "Input number of iterations: ";
     if(!(std::cin >> iterations) || iterations <= 0) throw std::logic_error("Invalid input");
 
-    switch(input) {
+    vector<double> result = {0.0};
+    switch(f_input) {
         case 1: {
-            //auto result = hillclimb(rastrigin, rastrigin_domain, rastrigin_p0, next_p_dist, iterations);
-            //std::cout << result << " -> " << rastrigin(result) << std::endl;
+            if(a_input == 1) {
+                auto result = hillclimb(rastrigin, rastrigin_domain, rastrigin_p0, next_p_dist, iterations);
+                std::cout << result << " -> " << rastrigin(result) << std::endl;
+            } else if(a_input == 2) {
+                auto result = annealing(rastrigin, rastrigin_domain, rastrigin_p0, iterations, normal, temperature);
+                std::cout << std::endl << result << " -> " << rastrigin(result) << std::endl;
+            }
             break;
         }
 
         case 2: {
-            //auto result = hillclimb(eggholder, eggholder_domain, eggholder_p0, next_p_dist, iterations);
-            //std::cout << result << " -> " << eggholder(result) << std::endl;
+            if(a_input == 1) {
+                auto result = hillclimb(eggholder, eggholder_domain, eggholder_p0, next_p_dist, iterations);
+                std::cout << result << " -> " << eggholder(result) << std::endl;
+            } else if(a_input == 2) {
+                auto result = annealing(eggholder, eggholder_domain, eggholder_p0, iterations, normal, temperature);
+                std::cout << std::endl << result << " -> " << eggholder(result) << std::endl;
+            }
             break;
         }
         default:
