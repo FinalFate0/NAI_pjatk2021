b5f434d98dfe2270bd26da28e63cbf2680b1eeb7
FinalFate0 <s20149@pjwstk.edu.pl>
2021-11-03 15:01:20 +0100

    started genotype to fenotype conversion

--- lab4/genetic1.cpp
+++ lab4/genetic1.cpp
@@ -4,9 +4,15 @@
 #include <vector>
 #include <random>
 #include <functional>
+#include <tuple>
+//#include <bit>
 
 using std::vector;
+using std::pair;
 using std::function;
+
+using gtype_t = vector<char>;
+using ftype_t = pair<double, double>;
 
 std::random_device rd;
 std::mt19937 gen(rd());
@@ -18,6 +24,41 @@ std::ostream& operator<<(std::ostream& o, vector<double> v)
     }
     return o;
 }
+
+
+//x: 10100101 -> x' = 165 -> real -> 5.12*(x'/(128))
+//y: 11011101 -> y' = 221 -> real -> 5.12*(y'/(128))
+
+/*
+
+for (int i = 0; i < 64;i++) {
+    x = (x << 1) + n[i];
+    y = (y << 1) + n[i+64];
+}
+
+
+*/
+
+ ftype_t gen_to_fen(gtype_t genotype) {
+    int temp_x = 0, temp_y = 0;
+
+
+    for (int i = 0; i < 64;i++) {
+        temp_x = (temp_x << 1);
+        temp_x += genotype[i];
+        temp_y = (temp_y << 1);
+        temp_y += genotype[i+64];
+    }
+    std::cout << temp_x << " " << temp_y << std::endl;
+
+    double x, y;
+    x = 5.12*temp_x/128;
+    y = 5.12*temp_y/128;
+
+    
+    return std::make_pair(x, y);
+ }
+
 
 
 int main() {
@@ -37,6 +78,26 @@ int main() {
         eggholder_dist(gen),
         eggholder_dist(gen)
     };
+
+
+    gtype_t genotype(128, 0);
+    genotype[127] = 1;
+    genotype[125] = 1;
+    genotype[124] = 1;
+    genotype[123] = 1;
+    genotype[122] = 1;
+    genotype[121] = 1;
+    genotype[120] = 1;
+
+    genotype[63] = 1;
+    genotype[61] = 1;
+    genotype[60] = 1;
+    genotype[59] = 1;
+    genotype[58] = 1;
+    genotype[57] = 1;
+    genotype[56] = 1;
+
+    std::cout << gen_to_fen(genotype).first << " " << gen_to_fen(genotype).second << std::endl;
 
     return 0;
 }
