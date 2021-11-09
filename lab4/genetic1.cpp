6c694ff0d8eb5109af627c0f8a675da3d1d71aab
Tymoteusz Urbanowicz <dym7774@gmail.com>
2021-11-09 18:06:25 +0100

    finished lab4

--- lab4/genetic1.cpp
+++ lab4/genetic1.cpp
@@ -26,6 +26,25 @@ std::ostream& operator<<(std::ostream& o, vector<double> v)
 }
 
 
+double fitness_egg(ftype_t fenotype) {
+    double target_x = 512.0, target_y = 404.2319;
+    double candidate_x = fenotype.first, candidate_y = fenotype.second;
+
+    double temp_x = (candidate_x+512.0)/(target_x+512.0);
+    double temp_y = (candidate_y+512.0)/(target_y+512.0);
+
+    if(temp_x > 1.0) {
+        temp_x -= 2.0*(temp_x-1.0);
+    }
+    if(temp_y > 1.0) {
+        temp_y -= 2.0*(temp_y-1.0);
+    }
+
+    double result = (temp_x+temp_y)/2.0;
+    return result;
+}
+
+
 //x: 10100101 -> x' = 165 -> real -> 5.12*(x'/(128))
 //y: 11011101 -> y' = 221 -> real -> 5.12*(y'/(128))
 
@@ -39,27 +58,48 @@ for (int i = 0; i < 64;i++) {
 
 */
 
- ftype_t gen_to_fen(gtype_t genotype) {
-    int temp_x = 0, temp_y = 0;
+ftype_t gen_to_fen(gtype_t genotype)
+{
+    long long temp_x = 0, temp_y = 0;
 
-
-    for (int i = 0; i < 64;i++) {
+    for (int i = 0; i < 64; i++)
+    {
         temp_x = (temp_x << 1);
         temp_x += genotype[i];
         temp_y = (temp_y << 1);
-        temp_y += genotype[i+64];
+        temp_y += genotype[i + 64];
     }
-    std::cout << temp_x << " " << temp_y << std::endl;
-
+    //std::cout << temp_x << " " << temp_y << std::endl;
     double x, y;
-    x = 5.12*temp_x/128;
-    y = 5.12*temp_y/128;
+    x = 512.000000000001 * (temp_x / pow(2, 63));
+    y = 512.000000000001 * (temp_y / pow(2, 63));
 
-    
     return std::make_pair(x, y);
- }
+}
 
+gtype_t fen_to_gen(ftype_t fenotype)
+{
+    gtype_t genotype(128, 0);
 
+    long long x = 0.0, y = 0.0;
+    x = (long long)((fenotype.first * pow(2, 63)) / 512.000000000001);
+    y = (long long)((fenotype.second * pow(2, 63)) / 512.000000000001);
+
+    //std::cout << x << " " << y << std::endl;
+    long long temp_x, temp_y;
+    for (int i = 63; i >= 0; i--)
+    {
+        
+        temp_x = (x & 1);
+        x = x >> 1;
+        genotype[i] = temp_x;
+        temp_y = (y & 1);
+        y = y >> 1;
+        genotype[i + 64] = temp_y;
+    }
+    
+    return genotype;
+}
 
 int main() {
 
@@ -79,25 +119,13 @@ int main() {
         eggholder_dist(gen)
     };
 
+    ftype_t test_fenotype = std::make_pair(512.0, 404.2319);
 
-    gtype_t genotype(128, 0);
-    genotype[127] = 1;
-    genotype[125] = 1;
-    genotype[124] = 1;
-    genotype[123] = 1;
-    genotype[122] = 1;
-    genotype[121] = 1;
-    genotype[120] = 1;
+    std::cout << std::fixed;
+    std::cout << std::setprecision(10);
 
-    genotype[63] = 1;
-    genotype[61] = 1;
-    genotype[60] = 1;
-    genotype[59] = 1;
-    genotype[58] = 1;
-    genotype[57] = 1;
-    genotype[56] = 1;
-
-    std::cout << gen_to_fen(genotype).first << " " << gen_to_fen(genotype).second << std::endl;
+    std::cout << gen_to_fen(fen_to_gen(test_fenotype)).first << " " << gen_to_fen(fen_to_gen(test_fenotype)).second << std::endl;
+    std::cout << "fitness:" << fitness_egg(test_fenotype) << std::endl; 
 
     return 0;
 }
