202f5fcf3a93028ddf7c5c7060e1c9e26e03b1ee
FinalFate0 <s20149@pjwstk.edu.pl>
2021-11-15 10:57:58 +0100

    added window.cpp

---
+++ lab6/window.cpp
@@ -0,0 +1,10 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/opencv_modules.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+int main( int argc, char** argv )
+{
+ cv::namedWindow("Okienko Prawie Inteligentne", 1);
+ cv::waitKey(0);
+ return 0;
+}
