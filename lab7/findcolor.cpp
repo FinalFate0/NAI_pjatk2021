3de52e54cf554614f7de4f60b10676b31f94e66c
Tymoteusz Urbanowicz <dym7774@gmail.com>
2021-11-23 14:31:47 +0100

    finished lab7

--- lab7/findcolor.cpp
+++ lab7/findcolor.cpp
@@ -11,6 +11,43 @@ int main(int argc, char** argv) {
 	int v1 = 0;
 	int v2 = 255;
 
+	int size_x = 320;
+	int size_y = 200;
+
+	for (int i = 1; i < argc; ++i) {
+		std::string arg = argv[i];
+		if (arg == "-x") {
+			if (i + 1 < argc) {
+				i++;
+				char* d;
+				size_x = std::strtol(argv[i], &d, 10);
+				if (*d) {
+					std::cerr << "-x must be followed by a positive number";
+					return 1;
+				}
+			}
+			else {
+				std::cerr << "-x option requires one argument" << std::endl;
+				return 1;
+			}
+		}
+		else if (arg == "-y") {
+			if (i + 1 < argc) {
+				i++;
+				char* d;
+				size_y = std::strtol(argv[i], &d, 10);
+				if (*d) {
+					std::cerr << "-y must be followed by a positive number";
+					return 1;
+				}
+			}
+			else {
+				std::cerr << "-y option requires one argument" << std::endl;
+				return 1;
+			}
+		}
+	}
+
 	VideoCapture cap1("nature.mp4");
 	if (!cap1.isOpened())
 		return -1;
@@ -23,18 +60,58 @@ int main(int argc, char** argv) {
 	createTrackbar("v1", "HSV", &v1, 255);
 	createTrackbar("v2", "HSV", &v2, 255);
 
+	int framecount = 0;
 	while (true) {
-		Mat og;
-		cap1.read(og);
-		GaussianBlur(og, og, Size(5, 5), 0);
-		resize(og, og, Size(320, 200));
+		Mat frame;
+		cap1.read(frame);
+		framecount++;
+
+		GaussianBlur(frame, frame, Size(5, 5), 0);
+		resize(frame, frame, Size(size_x, size_y));
 
 		Mat hsv;
-		cvtColor(og, hsv, COLOR_BGR2HSV);
+		cvtColor(frame, hsv, COLOR_BGR2HSV);
 
-		imshow("obraz", hsv);
+		Mat inrange;
+		inRange(hsv, Scalar(h1, s1, v1), Scalar(h2, s2, v2), inrange);
 
-		if (waitKey(1) == 27)
+		Mat output;
+		inrange.copyTo(output);
+		cvtColor(output, output, COLOR_GRAY2BGR);
+
+		putText(output,
+			"h1:" + std::to_string(h1) +
+			" h2:" + std::to_string(h2) +
+			" s1:" + std::to_string(s1) +
+			" s2:" + std::to_string(s2) +
+			" v1:" + std::to_string(v1) +
+			" v2:" + std::to_string(v2),
+			Point(10, 20),
+			FONT_HERSHEY_DUPLEX,
+			0.4,
+			Scalar(60, 70, 255),
+			1,
+			LINE_AA);
+
+		imshow("obraz", output);
+
+
+		if (framecount == cap1.get(CAP_PROP_FRAME_COUNT)) {
+			framecount = 0;
+			cap1 = VideoCapture("nature.mp4");
+		}
+
+		int key_pressed = waitKey(1);
+		if (key_pressed == 27) {
 			break;
+		}
+		else if (key_pressed == 120) {
+			Mat select;
+			inrange.copyTo(select);
+			imshow("zdjecie", select);
+			Rect roi_rect = selectROI("zdjecie", select);
+			Mat roi = Mat(select, roi_rect);
+			imwrite("output.jpg", roi);
+		}
 	}
 }
